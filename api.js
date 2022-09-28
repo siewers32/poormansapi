865482075aec4154dbe6605e0f244481c4ca500c
Jan Jaap Siewers <janjaap@siewers.org>
2022-09-28 17:02:31 +0200

    vanalles

---
+++ api.js
@@ -0,0 +1,95 @@
+
+const loginForm = document.getElementById('loginform')
+const movieForm = document.getElementById('getmovies')
+const logoutForm = document.getElementById('logout')
+loginForm.addEventListener('submit', login);
+movieForm.addEventListener('submit', getMovies);
+logoutForm.addEventListener('submit', logout);
+
+
+function getMovies(event) {       
+    result = getData('http://127.0.0.1:8000/api/movies-with-ratings', sessionStorage.getItem('token'))
+    .then((data) => {
+        console.log(data)
+    })
+    .catch(function(error) {
+        console.log('Request failed', error)
+    });
+    event.preventDefault();
+}
+
+function logout(event) {
+    console.log("in logout")
+    result = getData('http://127.0.0.1:8000/api/logout', sessionStorage.getItem('token'))
+    .then((data) => {
+        console.log(data)
+        sessionStorage.setItem("token", "44|WPjn7Ke40ZR60KwJrxt1UUiIUIXhQLntQJZ7p1xK")
+        console.log(sessionStorage.getItem("token"))
+        console.log(data)
+    })
+    .catch(function(error) {
+        console.log('Request failed', error)
+        console.log(sessionStorage.getItem("token"))
+    });
+    event.preventDefault();
+}
+
+function login(event) {
+    form = document.getElementById('loginform')
+    console.log(form.email.value)
+    console.log(form.password.value)
+    postData('http://localhost:8000/api/login', 
+    {
+        'email': form.email.value,
+        'password': form.password.value
+    })
+    .then((data) => {
+        console.log('Token from server: ' + data.token)
+        sessionStorage.setItem("token", data.token)
+        console.log('token from storage: ' + sessionStorage.getItem("token"))
+        result = getData('http://127.0.0.1:8000/api/movies-with-ratings', data.token)
+        .then((data) => {
+            console.log(data)
+        })
+        .catch(function(error) {
+            console.log('Request failed', error)
+        });
+    })
+    .catch(function(error) {
+        sessionStorage.setItem("token", "no-token")
+        console.log('Request failed', error)
+    });
+    event.preventDefault();
+}
+
+async function getData(url='', token) {
+    const response = await fetch(url,{
+        mode: 'cors',
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            //'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer ' + token,
+        },
+    })
+    return response.json();
+}
+
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+        'Content-Type': 'application/json',
+        //'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + sessionStorage.getItem("token"),
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
